9016fe7eacf5cb40d4dfb652020d6faebabb793a
Johannes Schneider <johannes-schneider_1995@web.de>
2020-11-17 18:06:12 +0100

    * add NullToVisibility converter

---
+++ FlatXaml/Converter/NullToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FlatXaml.Converter
+{
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public Visibility IsNullValue { get; set; } = Visibility.Collapsed;
+        public Visibility IsNotNullValue { get; set; } = Visibility.Visible;
+        
+        public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? IsNullValue : IsNotNullValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

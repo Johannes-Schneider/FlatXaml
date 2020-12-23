6966958b774e66e8f97fe29513df422379840c0b
Johannes Schneider <johannes.schneider@student.hpi.de>
2020-12-23 14:40:25 +0100

    add converter, which converts object type equality to a visibility

---
+++ FlatXaml/Converter/ObjectTypeEqualityToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FlatXaml.Converter
+{
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public class ObjectTypeEqualityToVisibilityConverter : IValueConverter
+    {
+        public Visibility TrueValue { get; set; } = Visibility.Visible;
+        public Visibility FalseValue { get; set; } = Visibility.Collapsed;
+        public Type? CompareTo { get; set; }
+        
+        public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.GetType() == CompareTo ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
